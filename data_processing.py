9ccd8fc4ad66fcccb7c2bfef23ecb6474c705508
mputak <mputak@tvz.hr>
2022-12-14 13:59:28 +0100

    Added elem_detec, write_to_file, minor changes to line_detector

--- data_processing.py
+++ data_processing.py
@@ -18,40 +18,32 @@ class Process:
         self.valid_wires = []
 
         self.result = model(self.images, size=640)
-        self.df = self.result.pandas().xywh  # removed [0] to have whole df in place
+        self.df = self.result.pandas().xywh
         self.df_xyxy = self.result.pandas().xyxy
 
-        for (image, df, df_xyxy) in zip(self.images, self.df, self.df_xyxy):  # added xyxy format for masking elements
+        for (image, df, df_xyxy) in zip(self.images, self.df, self.df_xyxy):
             ret, self.trashed = cv.threshold(image, self.threshold_value, 255, cv.THRESH_BINARY_INV)
             self.element_masker(image, df_xyxy)
-            # self.line_detector(self.junction_finder(df), image)
-
-        # self.result.show()
+            self.line_detector(self.junction_finder(df), image, padding=42)
+            self.element_detector(df)
+        self.write_to_file()
 
     @staticmethod
-    def junction_finder(df):  # new argument and made method static (please change that)
+    def junction_finder(df):
         '''Finds all junctions and outputs them in an array'''
-        junctions = df.loc[df['class'] == 4]  # removed self.df from both df in a line
+        junctions = df.loc[df['class'] == 4]
         junction_array = junctions[["xcenter", "ycenter"]].to_numpy()
 
         return junction_array.astype(int)
 
-    def element_masker(self, img, df): # new method for deleting elements
-
-        elements_array = df.to_numpy()
-        print(elements_array)
-        # all_elements = df.loc[df['class'] != 4]
-        mask = np.zeros(img.shape, dtype="uint8")
-        # array_of_elements = all_elements.to_numpy()
-
-        for row in elements_array:  # FIX!: make integers of floats
+    def element_masker(self, img, df):
+        '''Removes elements from the thresholded image'''
+        for row in df.to_numpy():
             if row[5] != 4:
-                cv.rectangle(mask, (int(row[0]), int(row[1])), (int(row[2]), int(row[3])), (255,255,255), -1) #int dont work
-        cv.imshow("Maska ones", mask)
+                cv.rectangle(self.trashed, (int(row[0]), int(row[1])), (int(row[2]), int(row[3])), (0, 0, 0), -1)
 
-    def line_detector(self, array, img):  # I am very certain OOP doesn't work like that
+    def line_detector(self, array, img, padding):
         '''Masks all ROI's and finds all lines'''
-        padding = 42  # padding for the ROI, 42 seems to work well as it's the number of the gods.
         junction_combinations = list(it.combinations(array, 2))
         for combination in junction_combinations:
             rect = cv.minAreaRect(np.asarray(combination))
@@ -67,39 +59,34 @@ class Process:
             mask = np.zeros(img.shape, dtype="uint8")
             cv.fillPoly(mask, [box], 255)
             masked = cv.bitwise_and(self.trashed, self.trashed, mask=mask)
-            lines = (cv.HoughLinesP(masked, rho=1, theta=np.pi / 180, threshold=50, minLineLength=50, maxLineGap=1))
-        # indent if under this to be in for loop to draw every line detection
-            if lines is not None:
-                # new_empty_list_of_valid_wires.append(combination) --> after all iterations, return this list
+            lines = (cv.HoughLinesP(masked, rho=1, theta=np.pi / 180, threshold=50, minLineLength=70, maxLineGap=4))
+            # cv.fillPoly(self.trashed, [box], 0)
+            if np.all(lines):
                 self.valid_wires.append(combination)
-                for i in range(0, len(lines)):
-                    l = lines[i][0]
-                    cv.line(self.trashed, (l[0], l[1]), (l[2], l[3]), (255, 255, 255), 5, cv.LINE_AA)
-                    
-        # return self.valid_wires
+                # for i in range(0, len(lines)):
+                #     l = lines[i][0]
+                #     cv.line(self.trashed, (l[0], l[1]), (l[2], l[3]), (255, 255, 255), 5, cv.LINE_AA)
+
         # testing purposes
         cv.imshow("THRASHED AND DETECTED", self.trashed)
-        cv.imshow("BITWISE", masked)
         cv.waitKey(0)
 
-    def nothing(self):
-        pass
+    def element_detector(self, df):
+        for row in df.to_numpy():
+            if row[5] != 4:
+                for wire in self.valid_wires:
+                    dx, dy = wire[1][0] - wire[0][0], wire[1][1] - wire[0][1]
+                    det = dx * dx + dy * dy
+                    a = (dy*(int(row[1]) - wire[0][1]) + dx * (int(row[0]) - wire[0][0])) / det
+                    pt = (wire[0][0] + a * dx, wire[0][1] + a * dy)
+                    cv.line(self.trashed, (int(row[0]), int(row[1])), (int(pt[0]), int(pt[1])), (255, 255, 255), 10)
+        cv.imshow("element", self.trashed)
+        cv.waitKey(0)
 
-        canny_edge = [cv.Canny(img, 100, 200) for img in self.images]
-        # cv.imshow("CANNY", canny_edge[0])
-        # (T, threshInv) = cv.threshold(self.images[0], 0, 255,
-        #                               cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+    def write_to_file(self):
+        fo = open("digitized_circuit.asc", "w")
+        fo.write("Version 4\nSHEET 1 880 680\n")
+        for wire in self.valid_wires:
+            fo.write(f"WIRE {wire[0][0]} {wire[0][1]} {wire[1][0]} {wire[1][1]}\n")
+            print(wire[0][0])
 
-        # thresh = cv.adaptiveThreshold(self.images[0], 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 19, 9)
-
-
-        '''
-        lines = cv.HoughLinesP(canny_edge[0], 1, np.pi / 180, 50, None, 50, 10)
-
-        if lines is not None:
-            for i in range(0, len(lines)):
-                l = lines[i][0]
-                cv.line(self.images[0], (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
-
-        cv.imshow("sussy", self.images[0])
-'''
